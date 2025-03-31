dd5413fbdffcbb6519fc89d1f2a4d67f9f509e33
L.chikhi <lounas.chikhi@etu.unistra.fr>
2025-03-31 23:41:27 +0200

    prise en compte des public-rooms

--- server/src/utils/lobbies.js
+++ server/src/utils/lobbies.js
@@ -2,17 +2,17 @@ import { Server } from "socket.io";
 import randomstring from "randomstring";
 
 const ID_LENGTH = 5;
-const NB_PLAYERS_MAX_IN_LOBBY = 10;
+const NB_PLAYERS_MAX_IN_ROOM = 10;
 
-class LobbyUser {
+class RoomUser {
     constructor(username, idSocketUser) {
     this.username = username;
     this.idSocketUser = idSocketUser;
   }
 }
 
-class Lobby {
-  constructor(id, host, idSocketHost, isPrivate = false) {
+class Room {
+    constructor(id, host, idSocketHost, isPrivate = false) {
     this.id = id;
     this.host = host;
     this.idSocketHost = idSocketHost;
@@ -20,125 +20,177 @@ class Lobby {
     this.private = isPrivate;
   }
 
-  addUser(username, idSocketUser) {
-    this.users.push(new LobbyUser(username, idSocketUser));
-  }
+    addUser(username, idSocketUser) {
+        this.users.push(new RoomUser(username, idSocketUser));
+    }
 
-  removeUser(idSocketUser) {
-    this.users = this.users.filter(user => user.idSocketUser !== idSocketUser);
-  }
+    removeUser(idSocketUser) {
+        this.users = this.users.filter(user => user.idSocketUser !== idSocketUser);
+    }
 
-  getUsernames() {
-    return this.users.map(user => user.username);
-  }
+    getUsernames() {
+        return this.users.map(user => user.username);
+    }
 
-  isFull() {
-    return this.users.length >= NB_PLAYERS_MAX_IN_LOBBY;
-  }
+    isFull() {
+        return this.users.length >= NB_PLAYERS_MAX_IN_Room;
+    }
 }
 
 export let rooms = [];
 
 export const roomHandler = (socket, io) => 
-{
-  const getAvailableRooms = () =>
-    rooms.filter(room => !room.private).map(room => room.id);
+{   
+        //fonction utile 
+    const getAvailableRooms = () => {
+        return rooms.filter(room => room.isPrivate === false).map(room => room.id);
+    };
 
-  const getUsers = (roomId) => {
-    const room = rooms.find(r => r.id === roomId);
-    return room ? room.getUsernames() : [];
-  };
 
-  socket.emit("available-rooms", getAvailableRooms());
+    const getUsers = (roomId) => {
+        const room = rooms.find(r => r.id === roomId);
+        return room ? room.getUsernames() : [];
+    };
 
-  const createRoom = (username) => {
-    const roomId = randomstring.generate({ length: ID_LENGTH, charset: "alphanumeric" });
-    const newRoom = new Lobby(roomId, username, socket.id);
-    newRoom.addUser(username, socket.id);
-    rooms.push(newRoom);
+    //fonctions principales
+    const createRoom = ({ username, isPrivate=false }) => 
+    {
+        const roomId = randomstring.generate({ length: ID_LENGTH, charset: "alphanumeric" });
+        const newRoom = new Room(roomId, username, socket.id, isPrivate); //on crée une instance de Room
+        newRoom.addUser(username, socket.id);       //on ajoute un user(host) dessus
+        rooms.push(newRoom);    //on push la nouvelle room (Room) dans le tableau des rooms globales
+        socket.join(roomId);
+        io.emit("available-rooms", getAvailableRooms());
+        if(isPrivate)
+        {
+            socket.emit("private-room-created", roomId);
+        }
+        else
+        {
+            socket.emit("public-room-created", roomId);
+        }
+        socket.emit("private-room-created", roomId);
+    };
 
-    socket.join(roomId);
-    io.emit("available-rooms", getAvailableRooms());
-    socket.emit("private-room-created", roomId);
-  };
+    const removeRoom = (roomId) => 
+    {
+        const room = rooms.find(r => r.id === roomId);
+        if (!room) return;
+        rooms = rooms.filter(r => r.id !== roomId);
+        if (room.isPrivate) 
+        {
+            io.to(roomId).emit("remove-private-room");
+        } 
+        else 
+        {
+            io.to(roomId).emit("remove-public-room");
+        }
+        io.emit("available-rooms", getAvailableRooms());
+    };
 
-  const removeRoom = (roomId) => {
-    rooms = rooms.filter(r => r.id !== roomId);
-    io.to(roomId).emit("remove-private-room");
-    io.emit("available-rooms", getAvailableRooms());
-  };
+    const joinRoom = ({ roomId, username }) => 
+    {
+        const room = rooms.find(r => r.id === roomId);
+        if (!room) 
+        {
+            socket.emit("room-not-found");
+            return;
+        }
+        if (room.isFull()) 
+        {
+            return false;
+        }
+        room.addUser(username, socket.id);
+        return room;
+    };
 
-  const joinRoom = ({ roomId, username }) => {
-    const room = rooms.find(r => r.id === roomId);
-    if (!room) {
-      socket.emit("private-room-not-found");
-      return;
-    }
-    if (room.isFull()) {
-      return false;
-    }
-    room.addUser(username, socket.id);
-    return true;
-  };
+    const leaveRoom = ({ roomId, host, socketId }) => 
+    {
+        const room = rooms.find(r => r.id === roomId);
+        if (!room) return;
+    
+        if (host) 
+        {
+            removeRoom(roomId);
+            socket.leave(roomId);
+            socket.emit("room-lefted");
+            return;
+        }
+        room.removeUser(socketId);
+        socket.leave(roomId);
+        socket.emit("room-lefted");
+    
+        const users = getUsers(roomId);
+        if (room.isPrivate)
+        {
+            socket.to(roomId).emit("user-left-private", users);
+        } 
+        else 
+        {
+            socket.to(roomId).emit("user-left-public", users);
+        }
+    };
 
-  const leaveRoom = ({ roomId, host, socketId }) => {
-    const room = rooms.find(r => r.id === roomId);
-    if (!room) return;
+    const leaveRoomWithSocketId = (socketId) => 
+        {
+            for (let room of rooms) 
+            {
+                if (room.idSocketHost === socketId) 
+                {
+                    removeRoom(room.id);
+                    return;
+                }
+          
+                const before = room.users.length;
+                room.removeUser(socketId);
+          
+                if (room.users.length < before) 
+                {
+                    const users = room.getUsernames();
+                    socket.leave(room.id);
+                    if (room.isPrivate) 
+                    {
+                        socket.to(room.id).emit("user-left-private", users);
+                    } 
+                    else 
+                    {
+                        socket.to(room.id).emit("user-left-public", users);
+                    }
+                    return;
+                }
+            }
+        };
 
-    if (host) {
-      removeRoom(roomId);
-      socket.to(roomId).emit("remove-private-room");
-      socket.leave(roomId);
-      socket.emit("private-room-lefted");
-      return;
-    }
 
-    room.removeUser(socketId);
-    socket.to(roomId).emit("user-left", getUsers(roomId));
-    socket.leave(roomId);
-    socket.emit("private-room-lefted");
-  };
+    //sockets listenners
+    socket.on("create-room", createRoom);
+    socket.on("leave-room", leaveRoom);
+    socket.on("disconnect", () => {leaveRoomWithSocketId(socket.id);});
 
-  const leaveRoomWithSocketId = (socketId) => {
-    for (let room of rooms) {
-      if (room.idSocketHost === socketId) {
-        removeRoom(room.id);
-        socket.to(room.id).emit("remove-private-room", getAvailableRooms());
-        socket.leave(room.id);
-        return;
-      }
+    socket.on("users-in-private-room", (roomId) => {
+        socket.emit("users-in-your-private-room", getUsers(roomId));});
 
-      const before = room.users.length;
-      room.removeUser(socketId);
-
-      if (room.users.length < before) {
-        socket.to(room.id).emit("user-left", getUsers(room.id));
-        socket.leave(room.id);
-        return;
-      }
-    }
-  };
-
-  // Sockets
-  socket.on("create-room", createRoom);
-
-  socket.on("users-in-private-room", (roomId) => {
-    socket.emit("users-in-your-private-room", getUsers(roomId));
-  });
-
-  socket.on("join-room", ({ roomId, username }) => {
-    const success = joinRoom({ roomId, username });
-    if (success) {
-      socket.join(roomId);
-      socket.emit("private-room-joined", getUsers(roomId));
-      socket.to(roomId).emit("users-in-your-private-room", getUsers(roomId));
-    } else if (success === false) {
-      socket.emit("private-room-is-full");
-    }
-  });
-
-  socket.on("leave-room", leaveRoom);
-  socket.on("disconnect", () => {
-    leaveRoomWithSocketId(socket.id);
-  });
+    socket.on("join-room", ({ roomId, username }) => 
+    {
+        const room = joinRoom({ roomId, username });
+        if (room) 
+        {
+            socket.join(roomId);
+            const users = getUsers(roomId);
+            if (room.isPrivate) 
+            {
+                socket.emit("private-room-joined", users);
+                socket.to(roomId).emit("users-in-your-private-room", users);
+            }
+            else
+            {
+                socket.emit("public-room-joined", users);
+                socket.to(roomId).emit("users-in-your-public-room", users);
+            }
+        } 
+        else
+        {
+            socket.emit("room-join-failed");
+        }
+    });
 };
