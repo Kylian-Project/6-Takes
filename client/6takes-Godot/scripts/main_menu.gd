432618aa1a1ea6fa1e9b91e94205a4771f85be63
Darren Delgado <darren.delgado@etu.unistra.fr>
2025-05-06 16:50:05 +0200

    Hides quit_button for web version

--- client/6takes-Godot/scripts/main_menu.gd
+++ client/6takes-Godot/scripts/main_menu.gd
@@ -34,6 +34,9 @@ var rules_instance = null
 var logged_in 
 
 func _ready() -> void:
+	if OS.get_name() == "Web":
+		quit_button.hide()
+	
 	rules_overlay.visible = false
 	settings_overlay.visible = false
 	singleplayer_button.pressed.connect(go_to_singleplayer)
